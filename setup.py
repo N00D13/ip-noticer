bfc5d520044714798ddcf94aff097e46308b0f00
Alex Wellnitz <alex@prointernetsMBP.fritz.box>
2019-05-07 20:13:11 +0200

    initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='ip-noticer',
+      version='0.1',
+      description='The funniest joke in the world',
+      url='https://github.com/N00D13/ip-noticer',
+      author='Alex Wellnitz',
+      author_email='moin@wellnitz-alex.de',
+      license='MIT',
+      packages=['ip-noticer'],
+      zip_safe=False)
